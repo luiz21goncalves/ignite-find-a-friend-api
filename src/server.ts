bf289168e44351337f1d195dfa413c0427453907
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-05-01 09:08:08 -0300

    chore: add graceful shutdown

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,17 @@ import fastify from 'fastify'
 
 const app = fastify()
 
-app.listen({ port: 3333, host: '0.0.0.0' }).then(console.log)
+app
+  .listen({ port: 3333, host: '0.0.0.0' })
+  .then((url) => console.log(`Server started at url:${url}`))
+
+function gracefulShutdown() {
+  console.log('server ending')
+  app.close(() => process.exit())
+}
+
+process.on('SIGINT', gracefulShutdown)
+process.on('SIGTERM', gracefulShutdown)
 
 process.on('uncaughtException', (error, origin) => {
   console.error(error, origin)
