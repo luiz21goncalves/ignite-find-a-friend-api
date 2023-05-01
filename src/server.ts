4db3bc3756151c6f6d25d39a89dadbbc42b881a8
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-05-01 08:52:38 -0300

    chore: add app global error handlers

--- src/server.ts
+++ src/server.ts
@@ -3,3 +3,11 @@ import fastify from 'fastify'
 const app = fastify()
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(console.log)
+
+process.on('uncaughtException', (error, origin) => {
+  console.error(error, origin)
+})
+
+process.on('unhandledRejection', (error) => {
+  console.error(error)
+})
