de3c9537163beeb5ae4ffbe20ee334b8a6345259
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-05-01 09:31:15 -0300

    chore: add custom logger

---
+++ src/logger/index.ts
@@ -0,0 +1,12 @@
+import pino, { LoggerOptions } from 'pino'
+
+const config: LoggerOptions = {
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+    },
+  },
+}
+
+export const logger = pino(config)
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import fastify from 'fastify'
 
-const app = fastify()
+import { logger } from './logger'
 
-app
-  .listen({ port: 3333, host: '0.0.0.0' })
-  .then((url) => console.log(`Server started at url:${url}`))
+const app = fastify({
+  logger,
+})
+
+app.listen({ port: 3333, host: '0.0.0.0' })
 
 function gracefulShutdown() {
-  console.log('server ending')
+  logger.info('Shutting down server')
   app.close(() => process.exit())
 }
 
@@ -15,9 +17,9 @@ process.on('SIGINT', gracefulShutdown)
 process.on('SIGTERM', gracefulShutdown)
 
 process.on('uncaughtException', (error, origin) => {
-  console.error(error, origin)
+  logger.error(error, origin)
 })
 
 process.on('unhandledRejection', (error) => {
-  console.error(error)
+  logger.error(error)
 })
