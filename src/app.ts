3af7c25ba10c9766ca94aef4510df5ae5f910c32
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-04-18 08:18:16 -0300

    feat: add uuid as request id

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
+import { randomUUID } from 'node:crypto'
+
 import cors from '@fastify/cors'
 import fastify from 'fastify'
 
 import { logger } from './logger'
 
-const app = fastify({ logger })
+const app = fastify({ genReqId: () => randomUUID(), logger })
 
 app.register(cors)
 
