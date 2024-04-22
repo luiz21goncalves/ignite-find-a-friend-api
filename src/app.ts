80183e2961b0f69a7071a72e63d2d87900688717
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-04-22 20:44:02 -0300

    chore: log errors

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,8 @@ app.setErrorHandler((fastifyError, request, replay) => {
   if (fastifyError instanceof ZodError) {
     const validationError = new ValidationError(fastifyError.format())
 
+    request.log.error(validationError.error)
+
     return replay
       .status(validationError.error.statusCode)
       .send(validationError.error)
@@ -27,10 +29,14 @@ app.setErrorHandler((fastifyError, request, replay) => {
   if (fastifyError instanceof AppError) {
     const appError = fastifyError.error
 
+    request.log.error(appError)
+
     return replay.status(appError.statusCode).send(appError)
   }
 
   const internalServerError = new InternalServerError()
+
+  request.log.error(fastifyError)
 
   return replay
     .status(internalServerError.error.statusCode)
