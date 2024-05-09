b11b86484e49c71d33e11476bd1b8dd7d17b8a60
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-05-09 07:43:42 -0300

    chore: add validation body params validation in create pets controller

--- src/http/controllers/pets/create-pet.ts
+++ src/http/controllers/pets/create-pet.ts
@@ -1,5 +1,19 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+import z from 'zod'
 
 export async function createPet(request: FastifyRequest, replay: FastifyReply) {
+  const createPetBodySchema = z.object({
+    about: z.string(),
+    age: z.string(),
+    dependency: z.string(),
+    kind: z.string(),
+    name: z.string(),
+    size: z.string(),
+    space: z.string(),
+  })
+
+  const { about, age, dependency, kind, name, size, space } =
+    createPetBodySchema.parse(request.body)
+
   return replay.status(201).send({})
 }
--- test/integration/v1/pets/post.spec.ts
+++ test/integration/v1/pets/post.spec.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker'
 import { CreateIdentityAndOrganization } from '@test/utils/create-identity-and-organization'
 import supertest from 'supertest'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
@@ -18,10 +19,26 @@ describe(`POST ${ROUTE}`, () => {
   it('should be able to create a pet', async () => {
     const { token } = await CreateIdentityAndOrganization()
 
+    const about = faker.lorem.sentence()
+    const age = faker.lorem.word()
+    const dependency = faker.lorem.word()
+    const name = faker.lorem.word()
+    const size = faker.lorem.word()
+    const kind = faker.lorem.word()
+    const space = faker.lorem.word()
+
     const response = await supertest(app.server)
       .post(ROUTE)
       .auth(token, { type: 'bearer' })
-      .send({})
+      .send({
+        about,
+        age,
+        dependency,
+        kind,
+        name,
+        size,
+        space,
+      })
 
     expect(response.status).toEqual(201)
     expect(response.body).toStrictEqual({})
@@ -35,6 +52,32 @@ describe(`POST ${ROUTE}`, () => {
       error: 'Authentication Required',
       message: 'Send a jwt token or re-authenticate',
       statusCode: 401,
+      type: 'validation_error',
+    })
+  })
+
+  it('should not be able to create a pet with invalid params', async () => {
+    const { token } = await CreateIdentityAndOrganization()
+
+    const response = await supertest(app.server)
+      .post(ROUTE)
+      .auth(token, { type: 'bearer' })
+      .send({})
+
+    expect(response.status).toEqual(400)
+    expect(response.body).toStrictEqual({
+      error: 'Validation failed',
+      message: {
+        _errors: [],
+        about: { _errors: ['Required'] },
+        age: { _errors: ['Required'] },
+        dependency: { _errors: ['Required'] },
+        kind: { _errors: ['Required'] },
+        name: { _errors: ['Required'] },
+        size: { _errors: ['Required'] },
+        space: { _errors: ['Required'] },
+      },
+      statusCode: 400,
       type: 'validation_error',
     })
   })
