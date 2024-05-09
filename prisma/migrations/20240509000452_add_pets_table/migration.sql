c09026cc9a5f43785df8baf1e58645af0ae26d3c
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-05-08 21:06:57 -0300

    chore: add pets model and migration

---
+++ prisma/migrations/20240509000452_add_pets_table/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "pets" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "about" TEXT NOT NULL,
+    "age" TEXT NOT NULL,
+    "dependency" TEXT NOT NULL,
+    "kind" TEXT NOT NULL,
+    "size" TEXT NOT NULL,
+    "space" TEXT NOT NULL,
+    "organization_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
